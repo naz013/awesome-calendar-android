f6a9d04b992c10c3c98937baf57cceddaaab8ed0
Nazar <n.suhovich@gmail.com>
2017-08-02 18:22:45 +0300

    Add list to showcase app;

--- app/src/main/java/com/github/naz013/awersomecalendar/MainActivity.java
+++ app/src/main/java/com/github/naz013/awersomecalendar/MainActivity.java
@@ -1,8 +1,14 @@
 package com.github.naz013.awersomecalendar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.github.naz013.awcalendar.MonthWeekView;
 
@@ -24,5 +30,37 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "onDateClicked: " + dateTime);
             }
         });
+
+        RecyclerView rv = findViewById(R.id.list_view);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setAdapter(new SimpleAdapter());
+    }
+
+    class SimpleAdapter extends RecyclerView.Adapter<SimpleAdapter.Holder> {
+
+        @Override
+        public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new Holder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(Holder holder, int position) {
+            holder.tv.setText("Item " + position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return 150;
+        }
+
+        class Holder extends RecyclerView.ViewHolder {
+
+            TextView tv;
+
+            Holder(View itemView) {
+                super(itemView);
+                tv = itemView.findViewById(R.id.text_view);
+            }
+        }
     }
 }
